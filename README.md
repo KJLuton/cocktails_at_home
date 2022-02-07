80b6dbe926daf5c55768ced4609aa77a31faf378
Kirstyn <kirstyn.jenna@hotmail.co.uk>
2022-02-07 19:31:22 +0000

    Added register page and form. Form validation and basic css in place.

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ Hero: Photo by <a href="https://unsplash.com/@meeuwesen?utm_source=unsplash&utm_
 Gin: 
 Photo by <a href="https://unsplash.com/@soulvanschaik?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Soul van Schaik</a> on <a href="https://unsplash.com/s/photos/gin-drinks?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
   
+Colours: 
+#d6d6d6 - grey
+#40b3b9 - blue
+#f2f2f2 - off white
+
+
--- run.py
+++ run.py
@@ -4,6 +4,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -32,6 +33,11 @@ def cocktails():
     cocktails = mongo.db.cocktails.find()
     return render_template("cocktails.html",
         cocktails=cocktails, page_title="COCKTAILS")
+
+
+@app.route("/register_mybar", methods=["GET", "POST"])
+def register_mybar():
+    return render_template("register_mybar.html")
 
 
 if __name__ == "__main__":
